81452cf2bb724ed257ca266fcda6717e500fb4cc
jizilong <jizilong1988@163.com>
2014-02-20 09:14:38 +0800

    output corrected words

--- lib/google_spell_checker.rb
+++ lib/google_spell_checker.rb
@@ -2,18 +2,14 @@
 require 'net/http'
 class GoogleSpellChecker
   API_BASEURI = "https://www.google.com.hk/search?"
+  SPELL_EXP = Regexp.new(/(\s|\w)*<em>(\w)*<\/em\>(\s|\w)*/)
   
   def self.check content
     params = {:q=> content, :oq=> content}
     uri = URI(API_BASEURI)
     uri.query = URI.encode_www_form(params)
     res = Net::HTTP.get(uri)
-    begin 
-      result = res.match(/>[\s*|\w*]*<em>\w*<\/em>/)[0].to_s
-      result[0]=""
-      result
-    rescue => e 
-      puts "#{e.class}: #{e.message}"
-    end
+    matches = res.to_enum(:scan,SPELL_EXP).map{Regexp.last_match}
+    matches[0,(matches.count/2)].inject(""){|result,match|result + match.to_s}
   end
 end
